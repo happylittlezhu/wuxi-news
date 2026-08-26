4a7d6b871c2555e6cd7e098d9d545add71e08ce3
happylittlezhu <958258116@qq.com>
2026-08-26 11:32:45 +0800

    域名迁移至GitHub Pages + 新增API部署脚本

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 App({
   globalData: {
-    apiUrl: 'https://87df0d8bdad34b7dbd63fec9585a8e32.app.workbuddy.link/data.json',
-    xlsxUrl: 'https://87df0d8bdad34b7dbd63fec9585a8e32.app.workbuddy.link/' + encodeURIComponent('无锡头部企业商业动态.xlsx')
+    apiUrl: 'https://happylittlezhu.github.io/wuxi-news/data.json',
+    xlsxUrl: 'https://happylittlezhu.github.io/wuxi-news/' + encodeURIComponent('无锡头部企业商业动态.xlsx')
   },
   onLaunch() {}
 });
